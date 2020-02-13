80d4469a2843d12cdc314150127bc66ea840a023
Dale Markowitz <dalequark@google.com>
2020-02-13 13:33:05 -0600

    added stream audio list and stt

--- DialogflowStream.js
+++ DialogflowStream.js
@@ -24,6 +24,7 @@ const Transform = require('readable-stream').Transform;
 const Speaker = require('speaker');
 const { PassThrough } = require('stream');
 const uuidv1 = require('uuid');
+const textToSpeech = require('@google-cloud/text-to-speech');
 
 const encoding = "LINEAR16";
 const sampleRateHertz = 16000;
@@ -61,19 +62,19 @@ class DialogflowStream {
         const detectStream = this.sessionClient
             .streamingDetectIntent()
             .on('error', console.error)
-    
+
         const recording = record
             .record({
-                sampleRateHertz: 16000,
+                sampleRateHertz: sampleRateHertz,
                 threshold: 0,
                 verbose: false,
                 recordProgram: 'sox', // Try also "arecord" or "sox"
                 silence: '10.0',
             });
-    
+
         const recordingStream = recording.stream()
             .on('error', console.error);
-        
+
         const pumpStream = pump(
             recordingStream,
             // Format the audio stream into the request format.
@@ -85,11 +86,11 @@ class DialogflowStream {
             }),
             detectStream
         );
-    
+
         let queryResult;
         return new Promise(resolve => {
             let silent = true
-    
+
             // Try to get them to say stuff
             detectStream.on('data', data => {
                 if (data.recognitionResult) {
@@ -101,7 +102,7 @@ class DialogflowStream {
                         console.log("Result Is Final");
                         recording.stop();
                     }
-                } 
+                }
                 if (data.queryResult) {
                     console.log(`Fulfillment text: ${data.queryResult.fulfillmentText}`);
                     queryResult = data.queryResult;
@@ -111,9 +112,9 @@ class DialogflowStream {
                     pumpStream.end();
                 }
             });
-    
+
             detectStream.write(this.makeInitialStreamRequestArgs(sessionId));
-    
+
            // ... or resolve after 5 seconds if they say nothing
            if (this.timeout) {
                 setTimeout(() => {
@@ -126,19 +127,19 @@ class DialogflowStream {
         })
     }
 
-    playAudio(audioBuffer) {
+    playAudio(audioBuffer, channels=1) {
         return new Promise(resolve => {
             // Setup the speaker for playing audio
             const speaker = new Speaker({
-                channels: 1,
+                channels: channels,
                 bitDepth: 16,
                 sampleRate: sampleRateHertz,
             });
-            
+
             speaker.on("close", () => {
                 resolve();
             });
-    
+
             // Setup the audio stream, feed the audio buffer in
             const audioStream = new PassThrough();
             audioStream.pipe(speaker);
@@ -146,29 +147,20 @@ class DialogflowStream {
         })
     }
 
+    // Given ssml (https://developers.google.com/assistant/actions/reference/ssml),
+    // calls the Text-to-Speech API and returns an audio data response.
+    async tts(ssml) {
+        const ttsClient = new textToSpeech.TextToSpeechClient();
+        // Construct the request
+        const request = {
+            input: { ssml: ssml },
+            voice: { languageCode: 'en-US', name: "en-US-Wavenet-D" },
+            audioConfig: { audioEncoding: 'LINEAR16', sample_rate_hertz: sampleRateHertz},
+        };
+        const [response] = await ttsClient.synthesizeSpeech(request);
+        return response["audioContent"];
+    }
+
 }
 
 module.exports = DialogflowStream;
-
-// async function stream() {
-//     console.log('Listening, press Ctrl+C to stop.');
-//     // Create a new id for this session
-//     const sessionId = uuidv1();
-    
-//     const stream = new DialogflowStream(YOUR_PROJECT_ID, TIMEOUT_SECONDS);
-
-//     let conversing = true;
-//     while (conversing) {
-//         const res = await stream.getAudio(sessionId);
-//         if (res["queryResult"]) {
-//             console.log("Got query result ", res["queryResult"]);
-//         }
-//         if (res["audio"]) {
-//             await stream.playAudio(res["audio"]);
-//         } else {
-//             conversing = false;
-//         }
-//     }
-// }
-
-// stream();
---
+++ stream_audio_list.js
@@ -0,0 +1,136 @@
+const uuidv1 = require('uuid/v1');
+const DialogflowStream = require('./DialogflowStream');
+//const moment = require('moment');
+//const textToSpeech = require('@google-cloud/text-to-speech');
+const keypress = require('keypress');
+const fs = require('fs');
+
+// Plays audioFile wav files through the speaker.
+// stream is a DialogflowStream object.
+async function playBuzzer(audioFile, stream) {
+    console.log("Buzz buzz buzz");
+    var contents = fs.readFileSync(audioFile);
+    await stream.playAudio(contents, 2);
+}
+
+function setTimer(stream, duration, unit) {
+    let durationMs;
+    switch(unit) {
+        case 's':
+            durationMs = duration * 1000;
+            break;
+        case 'min':
+            durationMs = duration * 1000 * 60;
+            break;
+        case 'h':
+            durationMs = duration * 1000 * 60**2;
+            break;
+        case 'day':
+            durationMs = duration * 1000 * 60**2 * 24;
+            break;
+        case 'year':
+            durationMs = duration * 1000 * 60**2 * 24 * 365;
+            break;
+        default:
+            return;
+    }
+    alarmStartTime = new Date();
+    alarmDurationMs = durationMs;
+    setTimeout(() => {playBuzzer("./alarms/spaceship_alarm.wav", stream)}, durationMs);
+}
+
+// Gets time remaining in the timer
+function getTimeRemaining() {
+    let currentTime = new Date();
+    if (!alarmStartTime || !alarmDurationMs) {
+        return null;
+    }
+    return (currentTime - alarmStartTime - alarmDurationMs) / 1000;
+}
+
+async function handleResponse(dfStream, audio, queryResult) {
+    // Once Dialogflow recognizes a user's intent, this
+    // function handles the response.
+    //
+    // `dfSteam` is passed so you can play audio with it's .playAudio method
+    // `audio` is an audio buffer returned by Dialogflow
+    // `queryResult` is data returned by Dialogflow, see
+    // https://cloud.google.com/dialogflow/docs/reference/rpc/google.cloud.dialogflow.v2#queryresult
+
+    const intent = queryResult.intent.displayName;
+    const parameters = queryResult.parameters["fields"];
+
+    console.log(`Recognized intent ${intent}`);
+    console.log(`Parameters`, parameters);
+
+    if (intent == "SetTimer" && parameters["duration"]) {
+        const duration = parameters["duration"]["structValue"]["fields"]["amount"]["numberValue"];
+        const unit = parameters["duration"]["structValue"]["fields"]["unit"]["stringValue"]; // s, min, h, day, yr
+        console.log(`Duration: ${duration} unit: ${unit}`)
+        setTimer(dfStream, duration, unit);
+
+    }
+    else if (intent == "timeLeft") {
+        let timeRemainingS = getTimeRemaining();
+        if(!timeRemainingS) {
+            audio = await dfStream.tts(`You have no timer set`);
+        }
+        else {
+            audio = await dfStream.tts(`You have ${timeRemainingS} seconds left on the timer`);
+        }
+        await dfStream.playAudio(audio, 2);
+        return;
+    }
+
+    if(audio) {
+        await dfStream.playAudio(audio, 1);
+    }
+
+    // The "end_conversation" flag lets us know whether we should expect
+    // the user to keep speaking after we play or response or whether
+    // the conversation is over and we can close the stream.
+    if (queryResult.diagnosticInfo && queryResult.diagnosticInfo["fields"]["end_conversation"]) {
+    	return false;
+    }
+    return true;
+}
+
+async function stream() {
+
+    console.log('Listening, press Ctrl+C to stop.');
+
+    // Create a new id for this session
+    const sessionId = uuidv1();
+
+    // Create a dialogflow stream that times out after 3 seconds
+    const stream = new DialogflowStream(process.env.PROJECT_ID, 3000);
+
+    let conversing = true;
+    while (conversing) {
+        const res = await stream.getAudio(sessionId);
+        if (res["audio"]) {
+            conversing = await handleResponse(stream, res["audio"], res["queryResult"]);
+        } else {
+            conversing = false;
+        }
+    }
+}
+
+let alarmStartTime;
+let alarmDurationMs;
+
+async function main() {
+	let inPress = false;
+	keypress(process.stdin);
+	process.stdin.on('keypress', async function (ch, key) {
+		console.log("Got key press");
+		if (inPress)	return;
+		inPress = true;
+		console.log("recording");
+		await stream();
+		console.log("done recording");
+		inPress = false;
+	    });
+}
+
+main();
